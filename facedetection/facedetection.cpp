4a69e2a2f2a93639d72cfd822676865ffc8e9083
Liu-rj <11911808@mail.sustech.edu.cn>
2020-12-25 17:00:22 +0800

    添加项目文件。

---
+++ facedetection/facedetection.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "facedetection.h"
+
+using namespace cv;
+
---
+++ facedetection/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "facedetection.h"
+
+using namespace cv;
+
+int main()
+{
+	Mat m = imread("./lena.jpg");
+}
