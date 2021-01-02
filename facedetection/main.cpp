152f96f09834466c217268057654ff9675ac4341
Liu-rj <11911808@mail.sustech.edu.cn>
2021-01-02 21:40:43 +0800

    can simply detect whether input image is a person

--- facedetection/main.cpp
+++ facedetection/main.cpp
@@ -1,10 +1,28 @@
+#pragma GCC optimize(3,"Ofast","inline")
+
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include "facedetection.h"
+#include <chrono>
 
 using namespace cv;
+using namespace std;
+using namespace chrono;
 
 int main()
 {
-	Mat m = imread("./lena.jpg");
+	Mat m = imread("./pics/bg.jpg");
+	Mat image;
+	m.convertTo(image, CV_32FC3);
+	float* img = convertRGB(image);
+	auto start = std::chrono::steady_clock::now();
+	float* result = cnn(img, m.rows, m.cols, m.channels());
+	auto end = std::chrono::steady_clock::now();
+	printf("%s%.4f\n", "Confidence score of background: ", result[0]);
+	cout << "---------------------------------------" << endl;
+	printf("%s%.4f\n", "Confidence score of face: ", result[1]);
+	cout << "---------------------------------------" << endl;
+	printf("%s%lld%s", "calculation takes ", duration_cast<std::chrono::milliseconds>(end - start).count(), " ms\n");
+	imshow("test_img", m);
+	waitKey(0);
 }
